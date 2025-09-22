a76f55ab28291af85ca8a036b3ba90e22378b220
Nebu <n.byju@shopper.com>
2025-09-22 17:13:20 +0530

    changes added

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -1,18 +1,8 @@
-// src/theme/Root.js
 import React from 'react';
-import { TinaProvider, TinaCMS, GitClient } from 'tinacms';
+import { TinaProvider, TinaCMS } from 'tinacms';
 
 export default function Root({ children }) {
-  const cms = React.useMemo(
-    () =>
-      new TinaCMS({
-        apis: {
-          git: new GitClient('/___tina'),
-        },
-        // Pass other configuration options here
-      }),
-    [],
-  );
+  const cms = React.useMemo(() => new TinaCMS({}), []);
 
   return <TinaProvider cms={cms}>{children}</TinaProvider>;
 }
